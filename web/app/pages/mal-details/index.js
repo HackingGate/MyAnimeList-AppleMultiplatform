bc896802af4794975b8d4a108c2d1e8af7fb8eb7
HackingGate <i@hackinggate.com>
2019-04-14 15:21:42 +0900

    Better name token

--- web/app/pages/mal-details/index.js
+++ web/app/pages/mal-details/index.js
@@ -17,58 +17,24 @@ var MalDetailsPage = ATV.Page.create({
 		ATV.Ajax
 			.get(API.malDetails(malId))
 			.then((xhr) => {
-				let name_token = xhr.response.title_english.toLowerCase().replaceAll(' ', '-');
-
+				let name_lower = xhr.response.title_english.toLowerCase();
+				let name_split = name_lower.split(':')[0];
+				let name_token = name_split.replaceAll('`', '').replaceAll(' ', '-');
 				// download webpage from crunchyroll
 				ATV.Ajax.get(CRAPI.webPage(name_token), {responseType: 'text'})
 					.then((crxhr) => {
-
-						// get seriesId from html
-						let page = crxhr.response
-						let idDivRegex = /<div class="show-actions" group_id="(.*)"><\/div>/; // search for a div with an id
-						var seriesId = page.match(idDivRegex)[1];
-						if (!seriesId) {
-							seriesId = 0;
-						}
-
-						let reqUrl = CRAPI.listCollections({
-        					series_id: seriesId,
-       						limit: 1000,
-        					offset: 0
-						});
-						ATV.Ajax
-							.get(reqUrl)
-							.then((crcollexhr) => {
-
-								let firstCollectionId = crcollexhr.response.data[0].collection_id;
-								
-								let reqUrl = CRAPI.listMedia({
-									collection_id: firstCollectionId,
-									include_clips: 0,
-									limit: 1000,
-									offset: 0
-								});
-								ATV.Ajax
-									.get(reqUrl)
-									.then((crmediaxhr) => {
-
-										let episodeArray = crmediaxhr.response.data;
-										resolve({
-											response: xhr.response,
-											episodes: episodeArray
-										});
-									}, (crmediaxhr) => {
-										// error
-										reject();
-									})						
-
-							}, (crcollexhr) => {
-								// error
-								reject();
-							})
+						getSeriesId(xhr, crxhr, resolve);
 					}, (crxhr) => {
 						// error
-						reject();
+						if (crxhr.status == 404) {
+
+						} else {
+							
+						}
+						resolve({
+							response: xhr.response,
+							debug: name_token
+						});
 					})
 
 				// resolve({
@@ -83,5 +49,58 @@ var MalDetailsPage = ATV.Page.create({
 			})
 	}
 });
+
+function getSeriesId(xhr, crxhr, resolve) {
+	// get seriesId from html
+	let page = crxhr.response
+	let idDivRegex = /<div class="show-actions" group_id="(.*)"><\/div>/; // search for a div with an id
+	var seriesId = page.match(idDivRegex)[1];
+	if (!seriesId) {
+		seriesId = 0;
+	}
+
+	let reqUrl = CRAPI.listCollections({
+		series_id: seriesId,
+		   limit: 1000,
+		offset: 0
+	});
+	ATV.Ajax
+		.get(reqUrl)
+		.then((crcollexhr) => {
+
+			let firstCollectionId = crcollexhr.response.data[0].collection_id;
+			
+			let reqUrl = CRAPI.listMedia({
+				collection_id: firstCollectionId,
+				include_clips: 0,
+				limit: 1000,
+				offset: 0
+			});
+			ATV.Ajax
+				.get(reqUrl)
+				.then((crmediaxhr) => {
+
+					let episodeArray = crmediaxhr.response.data;
+					resolve({
+						response: xhr.response,
+						episodes: episodeArray,
+						debug: reqUrl
+					});
+				}, (crmediaxhr) => {
+					// error
+					resolve({
+						response: xhr.response,
+						debug: 'crmediaxhr failed'
+					});
+				})						
+
+		}, (crcollexhr) => {
+			// error
+			resolve({
+				response: xhr.response,
+				debug: 'crcollexhr failed'
+			});
+		})
+}
 
 export default MalDetailsPage;
