c38583fc43e21147e631e1e677a24a4bce3558f1
HackingGate <i@hackinggate.com>
2020-07-09 23:20:44 +0900

    Create project with Xcode 12.0 beta 2

---
+++ MyAnimeList/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyAnimeList
+//
+//  Created by HG on 2020/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAnimeList/MyAnimeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAnimeListApp.swift
+//  MyAnimeList
+//
+//  Created by HG on 2020/07/09.
+//
+
+import SwiftUI
+
+@main
+struct MyAnimeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
