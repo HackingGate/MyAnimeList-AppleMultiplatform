7d24828233eb69899e48319fb1c9c30da77cbfe6
HackingGate <i@hackinggate.com>
2021-07-21 21:32:12 +0900

    Add macOS UITest

--- MyAnimeList (macOS) UITests/MyAnimeList__macOS__UITests.swift
+++ MyAnimeList (macOS) UITests/MyAnimeList__macOS__UITests.swift
@@ -1,13 +1,8 @@
-//
-//  MyAnimeListUITests.swift
-//  MyAnimeListUITests
-//
-//  Created by HG on 2021/07/16.
-//
+// swiftlint:disable type_name
 
 import XCTest
 
-class MyAnimeListUITests: XCTestCase {
+class MyAnimeList__macOS__UITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
