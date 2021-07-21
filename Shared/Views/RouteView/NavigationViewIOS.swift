f3b0767017bbc35694caaeebc4b7868f2797ad52
HackingGate <i@hackinggate.com>
2021-07-21 21:28:50 +0900

    Revert "Create my own StackNavigationView"
    This reverts commit d0cecb4154756f8b1d895b47284577ade866e68e.

--- Shared/Views/RouteView/NavigationViewIOS.swift
+++ Shared/Views/RouteView/NavigationViewIOS.swift
@@ -16,10 +16,6 @@ struct NavigationViewIOS<Content: View>: View {
             viewBuilder()
                 .navigationBarTitle(title)
         }
-        #elseif os(macOS)
-        StackNavigationView {
-            viewBuilder()
-        }
         #else
         viewBuilder()
         #endif
